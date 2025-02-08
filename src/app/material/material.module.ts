21e40190a83aaf29e2f25875443742fd8eed9103
david moreno <davidjmcxl@gmail.com>
2025-02-08 10:08:11 -0500

    feat: se crea la aplicacion y se agrega angular material

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSelectModule} from '@angular/material/select';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
+@NgModule({
+ exports:[
+  MatMenuModule,
+  MatIconModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatListModule,
+  MatDialogModule,
+  MatInputModule,
+  MatButtonModule,
+  MatFormFieldModule,
+  MatPaginatorModule,
+  MatTableModule,
+  MatSelectModule,
+  MatBadgeModule,
+  MatCheckboxModule,
+  MatSliderModule,
+  MatExpansionModule,
+  MatProgressSpinnerModule
+
+ ]
+})
+export class MaterialModule { }
